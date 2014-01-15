8a018fb5e98798a03b86b408fdabdcc8bc337097
annikol <anniken.olsen@gmail.com>
2014-01-15 12:04:32 +0100

    Create ov1.c

---
+++ ov1.c
@@ -0,0 +1,42 @@
+// gcc 4.7.2 +
+// gcc -std=gnu99 -Wall -g -o helloworld_c helloworld_c.c -lpthread
+
+#include <pthread.h>
+#include <stdio.h>
+
+int i = 0;
+
+// Note the return type: void*
+void* incr(){
+    for(int x = 0; x < 1000000; x++){
+        i++;
+    }
+    return NULL;
+}
+
+void* decr(){
+    for(int x = 0; x < 1000000; x++){
+        i--;
+    }
+    return NULL;
+}
+
+
+int main(){
+    pthread_t adder_thr;
+    pthread_create(&adder_thr, NULL, incr, NULL);
+
+    pthread_t decr_thr;
+    pthread_create(&decr_thr, NULL, decr, NULL);
+
+    /*for(int x = 0; x < 50; x++){
+        printf("%i\n", i);
+    }*/
+
+	pthread_join(adder_thr, NULL);
+	pthread_join(decr_thr, NULL);
+    printf("Done: %i\n", i);
+    return 0;
+
+    
+}
