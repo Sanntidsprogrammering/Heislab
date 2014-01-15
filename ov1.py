6f7da4e5c65b1a68a33f59afbe181bc228f96024
annikol <anniken.olsen@gmail.com>
2014-01-15 12:04:49 +0100

    Create ov1.py

---
+++ ov1.py
@@ -0,0 +1,36 @@
+# Python 3.3.3 and 2.7.6
+# python helloworld_python.py
+
+from threading import Thread
+
+i = 0
+
+def adder():
+# In Python you "import" a global variable, instead of "export"ing it when you declare it
+    global i
+    
+# In Python 2 this generates a list of integers (which takes time),
+# while in Python 3 this is an iterable (which is much faster to generate).
+# In python 2, an iterable is created with xrange()
+    for x in range(0, 1000000):
+        i += 1
+
+def decr():
+
+    global i
+
+    for x in range(0, 1000000):
+        i -= 1	
+
+
+def main():
+    adder_thr = Thread(target = adder)
+    adder_thr.start()
+    decr_thr = Thread(target = decr)
+    decr_thr.start()
+
+    adder_thr.join()
+    decr_thr.join()
+    print("Done: " + str(i))
+
+main()
